39d029cc5da323e02e1ccf15cdbf8dc0b9edb91f
jkaramon <karamon@softcamp.cz>
2011-08-04 10:41:53 +0300

    Temporarily disabling embedded document callbacks because of https://github.com/jnunemaker/mongomapper/issues/265

--- lib/mongo_mapper/plugins/embedded_callbacks.rb
+++ lib/mongo_mapper/plugins/embedded_callbacks.rb
@@ -18,13 +18,13 @@ module MongoMapper
             embedded_docs += Array(get_proxy(association).send(:load_target))
           end
 
-          block = embedded_docs.inject(block) do |chain, doc|
-            if doc.class.respond_to?("_#{callback}_callbacks")
-              lambda { doc.run_callbacks(callback, &chain) }
-            else
-              chain
-            end
-          end
+         #  block = embedded_docs.inject(block) do |chain, doc|
+         #   if doc.class.respond_to?("_#{callback}_callbacks")
+         #     lambda { doc.run_callbacks(callback, &chain) }
+         #   else
+         #     chain
+         #   end
+         # end
           super callback, &block
         end
       end
